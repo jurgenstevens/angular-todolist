8777fb99e77bd31e48ccdfaa925cca7d33d5ba85
Jurgen <jurgen.stevens@yahoo.com>
2020-04-11 14:22:49 -0400

    Created new component for each individual todo item called todo-item.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TodosComponent } from './components/todos/todos.component';
+import { TodoItemComponent } from './components/todo-item/todo-item.component';
 
 // this is the root app module
 @NgModule({
   // this is where components are gonna go. New components must be added here as well
-  declarations: [AppComponent, TodosComponent],
+  declarations: [AppComponent, TodosComponent, TodoItemComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
