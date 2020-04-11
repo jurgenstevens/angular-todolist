6997f5b8c9a970a612c738983e891bee6b3d5ab2
Jurgen <jurgen.stevens@yahoo.com>
2020-04-11 18:57:58 -0400

    Added todo brackets in app-todo-item to passing a property for each todo item.

--- src/app/components/todo-item/todo-item.component.ts
+++ src/app/components/todo-item/todo-item.component.ts
@@ -1,15 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
+  // this will be passed through todos.component.html
   selector: 'app-todo-item',
   templateUrl: './todo-item.component.html',
-  styleUrls: ['./todo-item.component.css']
+  styleUrls: ['./todo-item.component.css'],
 })
 export class TodoItemComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
