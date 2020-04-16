df5910a6d9fd9d61930c3c7dc690b73bae76b930
Jurgen <jurgen.stevens@yahoo.com>
2020-04-15 23:43:30 -0500

    Added a limit of 5 with a new property called todosLimit.

--- src/app/services/todo.service.ts
+++ src/app/services/todo.service.ts
@@ -10,12 +10,13 @@ import { Todo } from '../models/Todo';
 })
 export class TodoService {
   todosUrl: string = 'https://jsonplaceholder.typicode.com/todos';
+  todosLimit: string = '?_limit=5';
 
   constructor(private http: HttpClient) {}
 
   // this get request will take in the todos url and observable from rxjs
   getTodos(): Observable<Todo[]> {
-    // this is gonna be in todo type and will be an array
-    return this.http.get<Todo[]>(this.todosUrl);
+    // this is the get request to the url above AND the limit which is five todos
+    return this.http.get<Todo[]>(`${this.todosUrl}${this.todosLimit}`);
   }
 }
