d7fdc35aab31a2863276e87f82263c7b93b210e5
Jurgen <jurgen.stevens@yahoo.com>
2020-04-17 23:56:53 -0500

    Added the input for the add-todo component.

--- src/app/components/add-todo/add-todo.component.ts
+++ src/app/components/add-todo/add-todo.component.ts
@@ -3,13 +3,13 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-add-todo',
   templateUrl: './add-todo.component.html',
-  styleUrls: ['./add-todo.component.css']
+  styleUrls: ['./add-todo.component.css'],
 })
 export class AddTodoComponent implements OnInit {
+  // title input
+  title: string;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
