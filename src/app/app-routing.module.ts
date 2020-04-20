47758e416d612500278b10c29677298d219f9e75
Jurgen <jurgen.stevens@yahoo.com>
2020-04-19 23:34:54 -0500

    Imported AboutComponent to app-routing module.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { TodosComponents } from './components/todos/todos.component';
+import { About Component } from './components/pages/about.component';
+import { AboutComponent } from './components/pages/about/about.component';
 
-const routes: Routes = [{ path: '', component: TodosComponents }];
+const routes: Routes = [
+  { path: '', component: TodosComponents },
+  { path: 'about', component: AboutComponent;}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
